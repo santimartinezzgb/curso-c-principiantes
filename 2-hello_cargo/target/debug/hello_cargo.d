4a05d083cc3f24f0ce035e0ea132823ab16825d1
santimartinezzgb <santimgb98@gmail.com>
2025-12-12 23:42:36 +0100

    Remove tracked target directories

--- 2-hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/santi/curso-c-principiantes/hello_cargo/target/debug/hello_cargo: /home/santi/curso-c-principiantes/hello_cargo/src/main.rs
--- 3-guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/home/santi/rust-learning/3-guessing_game/target/debug/guessing_game: /home/santi/rust-learning/3-guessing_game/src/main.rs
